b62dfcb6fa36f0179a40c2e91ac5ce5d5fc5a4a2
Angel ILIEV <a.v.iliev13@gmail.com>
2019-08-04 12:45:43 +0300

    Added ability to get moves from slinky and parse them.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import subprocess
+
 import pygame
 from defines import *
 
@@ -93,13 +95,14 @@ class Game:
 
                 if event.type == pygame.MOUSEBUTTONUP:
                     sq_idx = self.get_square_under_mouse(event.pos)
+                    moves = self.get_allowed_moves(sq_idx)
 
                     if clicked_square_idx is not None and sq_idx != clicked_square_idx:
                         self.move_piece(clicked_square_idx, sq_idx)
                         clicked_square_idx = None
                     else:
-                        clicked_square_idx = sq_idx
-                        self.get_allowed_moves(sq_idx)
+                        if moves:  # I have allowed moves for this square -> set it as clicked
+                            clicked_square_idx = sq_idx
 
             self.canvas.fill(pygame.Color('black'))
 
@@ -127,11 +130,28 @@ class Game:
         # this is used to track evey move since starting position
         self.move_history.append(self.get_move_str(from_sq, to_sq))
 
+    def parse_engine_moves(self, moves_str: str):
+        idx = moves_str.find("Moves found:")  # find start of moves string
+        moves_str = moves_str[idx:]  # remove everything before that from string
+        idx = moves_str.find("->")  # find delimiter where moves start
+        moves_str = moves_str[idx + len("->"):].strip()  # remove everything before it
+        moves = moves_str.split(",")  # split moves based on commas
+        moves = map(lambda m: m.strip(), moves)  # remove extra whitespaces from move str
+        return filter(lambda x: x != '', moves)  # return all non empty strings
+
     def get_allowed_moves(self, sq):
-        # todo exec command
-        # todo parse stdin response
-        # todo filter moves that start with square_str
-        pass
+        def is_start_square(move_):
+            # check if sq matches the from part of move notation
+            return self.get_sq_str(sq) in move_[:2]
+
+        # todo instead of sending new, start sending what UCI protocol does
+        # todo i.e. position startpos d2d4 (position indicates new position, startpos means start,
+        #  todo d2d4... all moves after start pos)
+        engine = subprocess.run(["slinky.exe", "new, getmoves"], stdout=subprocess.PIPE)
+        output = engine.stdout.decode('utf-8')
+        moves = self.parse_engine_moves(output)
+        moves_for_square = list(filter(is_start_square, moves))
+        return moves_for_square
 
     @classmethod
     def get_move_str(cls, from_sq, to_sq):

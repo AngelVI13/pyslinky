dd74abf51cd80be85a535545a06350cf7e3551b9
Angel ILIEV <a.v.iliev13@gmail.com>
2019-08-04 13:46:24 +0300

    Added dicts to map piece to img path and piece to img size. Then added dynamically loading all images and scaling them based on these dicts.

--- defines.py
+++ defines.py
@@ -25,3 +25,34 @@ START_POS = [
 ]
 
 RANK_CHAR = dict(zip(range(ROWS), ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']))
+
+IMAGE_PATHS = {
+    BPAWN: 'assets/b_pawn_png_128px.png',
+    WPAWN: 'assets/w_pawn_png_128px.png',
+    BKNIGHT: 'assets/b_knight_png_128px.png',
+    WKNIGHT: 'assets/w_knight_png_128px.png',
+    BBISHOP: 'assets/b_bishop_png_128px.png',
+    WBISHOP: 'assets/w_bishop_png_128px.png',
+    BROOK: 'assets/b_rook_png_128px.png',
+    WROOK: 'assets/w_rook_png_128px.png',
+    BQUEEN: 'assets/b_queen_png_128px.png',
+    WQUEEN: 'assets/w_queen_png_128px.png',
+    BKING: 'assets/b_king_png_128px.png',
+    WKING: 'assets/w_king_png_128px.png',
+}
+
+IMAGE_SIZES = {
+    #    : (w, h)
+    BPAWN: (48, 58),
+    WPAWN: (48, 58),
+    BKNIGHT: (52, 58),
+    WKNIGHT: (52, 58),
+    BBISHOP: (58, 58),
+    WBISHOP: (58, 58),
+    BROOK: (53, 58),
+    WROOK: (53, 58),
+    BQUEEN: (58, 52),
+    WQUEEN: (58, 52),
+    BKING: (58, 58),
+    WKING: (58, 58),
+}
--- main.py
+++ main.py
@@ -28,63 +28,14 @@ class Game:
         self.light_square = pygame.transform.scale(light_square, (SQUARE_SIZE, SQUARE_SIZE))
         self.highlight_square = pygame.transform.scale(highlight_square , (SQUARE_SIZE, SQUARE_SIZE))
 
-        self.piece_images = {}
-        # -- Pawns
-        # todo are member variables needed ?
-        black_pawn = pygame.image.load('assets/b_pawn_png_128px.png')
-        white_pawn = pygame.image.load('assets/w_pawn_png_128px.png')
-        # todo move scaling values to dict so that they can be used later
-        # todo to determine the padding from top and bottom needed
-        self.black_pawn = pygame.transform.scale(black_pawn, (48, 58))
-        self.white_pawn = pygame.transform.scale(white_pawn, (48, 58))
+        self.piece_images = self.load_assets()
 
-        self.piece_images[BPAWN] = self.black_pawn
-        self.piece_images[WPAWN] = self.white_pawn
-
-        # -- Knights
-        black_knight = pygame.image.load('assets/b_knight_png_128px.png')
-        white_knight = pygame.image.load('assets/w_knight_png_128px.png')
-        self.black_knight = pygame.transform.scale(black_knight, (52, 58))
-        self.white_knight = pygame.transform.scale(white_knight, (52, 58))
-
-        self.piece_images[BKNIGHT] = self.black_knight
-        self.piece_images[WKNIGHT] = self.white_knight
-
-        # -- Bishops
-        black_bishop = pygame.image.load('assets/b_bishop_png_128px.png')
-        white_bishop = pygame.image.load('assets/w_bishop_png_128px.png')
-        self.black_bishop = pygame.transform.scale(black_bishop, (58, 58))
-        self.white_bishop = pygame.transform.scale(white_bishop, (58, 58))
-
-        self.piece_images[BBISHOP] = self.black_bishop
-        self.piece_images[WBISHOP] = self.white_bishop
-
-        # -- Rooks
-        black_rook = pygame.image.load('assets/b_rook_png_128px.png')
-        white_rook = pygame.image.load('assets/w_rook_png_128px.png')
-        self.black_rook = pygame.transform.scale(black_rook, (53, 58))
-        self.white_rook = pygame.transform.scale(white_rook, (53, 58))
-
-        self.piece_images[BROOK] = self.black_rook
-        self.piece_images[WROOK] = self.white_rook
-
-        # -- Queens
-        black_queen = pygame.image.load('assets/b_queen_png_128px.png')
-        white_queen = pygame.image.load('assets/w_queen_png_128px.png')
-        self.black_queen = pygame.transform.scale(black_queen, (58, 52))
-        self.white_queen = pygame.transform.scale(white_queen, (58, 52))
-
-        self.piece_images[BQUEEN] = self.black_queen
-        self.piece_images[WQUEEN] = self.white_queen
-
-        # -- Kings
-        black_king = pygame.image.load('assets/b_king_png_128px.png')
-        white_king = pygame.image.load('assets/w_king_png_128px.png')
-        self.black_king = pygame.transform.scale(black_king, (58, 58))
-        self.white_king = pygame.transform.scale(white_king, (58, 58))
-
-        self.piece_images[BKING] = self.black_king
-        self.piece_images[WKING] = self.white_king
+    @staticmethod
+    def load_assets():
+        # load all images
+        d = {k: pygame.image.load(v) for k, v in IMAGE_PATHS.items()}
+        # scale them
+        return {k: pygame.transform.scale(v, IMAGE_SIZES[k]) for k, v in d.items()}
 
     def run(self):
         clicked_square_idx = None
@@ -176,7 +127,10 @@ class Game:
     def draw_pos(self):
         for idx, piece in enumerate(self.pos):
             if piece != EMPTY:
-                self.canvas.blit(self.piece_images[piece], self.square_loc[idx])
+                w, h = self.square_loc[idx]
+                image_w, image_h = IMAGE_SIZES[piece]
+                padding_w, padding_h = (SQUARE_SIZE - image_w) // 2, (SQUARE_SIZE - image_h) // 2
+                self.canvas.blit(self.piece_images[piece], (w + padding_w, h + padding_h))
 
     def draw_clicked_square(self, sq):
         if sq is not None:
